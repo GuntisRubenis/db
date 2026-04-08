5a904f8c301c359bd89f283c2b0fc8b33ac7f97a
Guntis <guntis.rubenis.wa@gmail.com>
2026-04-08 10:32:37 +0300

    Adding simple statements

--- main.c
+++ main.c
@@ -27,12 +27,10 @@ ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 
 void read_input(InputBuffer* input_buffer){
     ssize_t bytes_read =  getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
-    
     if(bytes_read <= 0){
         printf("Error reaing input\n");
         exit(EXIT_FAILURE);
     }
-
     //Ignore trailing newline
     input_buffer->input_length = bytes_read -1;
     input_buffer->buffer[bytes_read -1] = 0;
@@ -40,10 +38,68 @@ void read_input(InputBuffer* input_buffer){
 }
 
 void close_input_buffer(InputBuffer* input_buffer){
-
     free(input_buffer->buffer);
     free(input_buffer);
 }
+
+
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer){
+    if(strcmp(input_buffer->buffer, ".exit") == 0){
+        exit(EXIT_SUCCESS);
+    } else{
+        return META_UNRECOGNIZED_COMMAND;
+    }
+}
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+    StatementType type;
+
+} Statement;
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0){
+
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS; 
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0){
+
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS; 
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement){
+    switch (statement->type){
+        case (STATEMENT_INSERT):
+            printf("This is where we would do insert.\n");
+            break;
+        case (STATEMENT_SELECT):
+            printf("This is where we would do select.\n");
+            break;
+
+    }
+    
+}
+
+
 
 int main(int argc, char * argv[]){
 
@@ -55,12 +111,28 @@ int main(int argc, char * argv[]){
         print_prompt();
         read_input(input_buffer);
 
-        if(strcmp(input_buffer->buffer, ".exit") == 0){
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        } else{
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        if(input_buffer->buffer[0] == '.'){
+
+            switch (do_meta_command(input_buffer)){
+              case (META_COMMAND_SUCCESS):
+                continue;
+              case (META_UNRECOGNIZED_COMMAND):
+                printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                continue;
+            }
         }
+
+        Statement statement;
+        switch(prepare_statement(input_buffer, &statement)){
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start of '%s'\n", input_buffer->buffer);
+                continue;
+        }
+        execute_statement(&statement);
+        printf("Executed.\n");
+
     }
     
     return 0; 
